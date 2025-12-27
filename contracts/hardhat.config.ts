2baf70f60c9a95bd3d5a1e451fe57dae27adf671
ChaiGans <elbertchailes888@gmail.com>
2025-12-27 23:47:05 +0700

    feat: add demo script, to show how ethers querying/tx-ign

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -19,7 +19,10 @@ const config: HardhatUserConfig = {
       url: process.env.POLYGON_AMOY_RPC || "",
       accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
     },
-
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+    },
   },
   // etherscan: {
   //   apiKey: process.env.ETHERSCAN_API_KEY || "",
